69941235e4353c69fdce8828edb5c253fe5b6b8e
lingjiamian <836376242@qq.com>
2018-09-10 10:27:25 +0800

    提交项目

---
+++ 鲜花订购与管理系统/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 鲜花订购与管理系统.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
